2471ab4cd52c321fdd7bf48d7c5a151194d370b5
karma.wang <karma.wang@cloudminds.com>
2020-08-05 14:53:19 +0800

    完善脚本，添加说明
    Bug-Id:
    Depends-On: N/A

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ fi
 ### Optional parameters ###
 # To quickly test that model training works, set the number of epochs to a
 # smaller value (e.g. 0.01).
-export DOMAIN_NAME=$2  # "phone_call" #　"navigation" #
+export DOMAIN_NAME="music"
 export input_format="nlu"
 export num_train_epochs=5
 export TRAIN_BATCH_SIZE=100
@@ -40,61 +40,61 @@ export entity_type_list_file=${OUTPUT_DIR}/entity_type_list.json
 export NUM_TRAIN_EXAMPLES=300000
 export NUM_EVAL_EXAMPLES=5000
 
-#python preprocess_main.py \
-#    --input_file=${CORPUS_DIR}/train.txt \
-#    --input_format=${input_format} \
-#    --output_tfrecord_train=${OUTPUT_DIR}/train.tf_record \
-#    --output_tfrecord_dev=${OUTPUT_DIR}/dev.tf_record \
-#    --label_map_file=${label_map_file} \
-#    --slot_label_map_file=${slot_label_map_file} \
-#    --vocab_file=${BERT_BASE_DIR}/vocab.txt \
-#    --max_seq_length=${max_seq_length} \
-#    --do_lower_case=${do_lower_case} \
-#    --domain_name=${DOMAIN_NAME} \
-#    --entity_type_list_file=${entity_type_list_file}
+python preprocess_main.py \
+    --input_file=${CORPUS_DIR}/train.txt \
+    --input_format=${input_format} \
+    --output_tfrecord_train=${OUTPUT_DIR}/train.tf_record \
+    --output_tfrecord_dev=${OUTPUT_DIR}/dev.tf_record \
+    --label_map_file=${label_map_file} \
+    --slot_label_map_file=${slot_label_map_file} \
+    --vocab_file=${BERT_BASE_DIR}/vocab.txt \
+    --max_seq_length=${max_seq_length} \
+    --do_lower_case=${do_lower_case} \
+    --domain_name=${DOMAIN_NAME} \
+    --entity_type_list_file=${entity_type_list_file}
 
 
 
-#echo "Train the model."
-#python run_lasertagger.py \
-#  --training_file=${OUTPUT_DIR}/train.tf_record \
-#  --eval_file=${OUTPUT_DIR}/dev.tf_record \
-#  --label_map_file=${label_map_file} \
-#  --slot_label_map_file=${slot_label_map_file} \
-#  --model_config_file=${CONFIG_FILE} \
-#  --output_dir=${MODEL_DIR} \
-#  --init_checkpoint=${BERT_BASE_DIR}/bert_model.ckpt \
-#  --do_train=true \
-#  --do_eval=true \
-#  --num_train_epochs=${num_train_epochs} \
-#  --train_batch_size=${TRAIN_BATCH_SIZE} \
-#  --learning_rate=${learning_rate} \
-#  --warmup_proportion=${warmup_proportion} \
-#  --drop_keep_prob=${drop_keep_prob} \
-#  --kernel_size=${kernel_size}  \
-#  --save_checkpoints_steps=${SAVE_CHECKPOINT_STEPS} \
-#  --max_seq_length=${max_seq_length} \
-#  --num_train_examples=${NUM_TRAIN_EXAMPLES} \
-#  --num_eval_examples=${NUM_EVAL_EXAMPLES} \
-#  --domain_name=${DOMAIN_NAME} \
-#  --entity_type_list_file=${entity_type_list_file}
+echo "Train the model."
+python run_lasertagger.py \
+  --training_file=${OUTPUT_DIR}/train.tf_record \
+  --eval_file=${OUTPUT_DIR}/dev.tf_record \
+  --label_map_file=${label_map_file} \
+  --slot_label_map_file=${slot_label_map_file} \
+  --model_config_file=${CONFIG_FILE} \
+  --output_dir=${MODEL_DIR} \
+  --init_checkpoint=${BERT_BASE_DIR}/bert_model.ckpt \
+  --do_train=true \
+  --do_eval=true \
+  --num_train_epochs=${num_train_epochs} \
+  --train_batch_size=${TRAIN_BATCH_SIZE} \
+  --learning_rate=${learning_rate} \
+  --warmup_proportion=${warmup_proportion} \
+  --drop_keep_prob=${drop_keep_prob} \
+  --kernel_size=${kernel_size}  \
+  --save_checkpoints_steps=${SAVE_CHECKPOINT_STEPS} \
+  --max_seq_length=${max_seq_length} \
+  --num_train_examples=${NUM_TRAIN_EXAMPLES} \
+  --num_eval_examples=${NUM_EVAL_EXAMPLES} \
+  --domain_name=${DOMAIN_NAME} \
+  --entity_type_list_file=${entity_type_list_file}
 
 
-### 4. Prediction
+## 4. Prediction
 
-#### Export the model.
-#echo "Export the model."
-#python run_lasertagger.py \
-#  --label_map_file=${label_map_file} \
-#  --slot_label_map_file=${slot_label_map_file} \
-#  --model_config_file=${CONFIG_FILE} \
-#  --max_seq_length=${max_seq_length} \
-#  --kernel_size=${kernel_size}  \
-#  --output_dir=${MODEL_DIR}  \
-#  --do_export=true \
-#  --export_path="${MODEL_DIR}/export" \
-#  --domain_name=${DOMAIN_NAME} \
-#  --entity_type_list_file=${entity_type_list_file}
+### Export the model.
+echo "Export the model."
+python run_lasertagger.py \
+  --label_map_file=${label_map_file} \
+  --slot_label_map_file=${slot_label_map_file} \
+  --model_config_file=${CONFIG_FILE} \
+  --max_seq_length=${max_seq_length} \
+  --kernel_size=${kernel_size}  \
+  --output_dir=${MODEL_DIR}  \
+  --do_export=true \
+  --export_path="${MODEL_DIR}/export" \
+  --domain_name=${DOMAIN_NAME} \
+  --entity_type_list_file=${entity_type_list_file}
 
 ######### Get the most recently exported model directory.
 TIMESTAMP=$(ls "${MODEL_DIR}/export/" | \
